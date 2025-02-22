fb2cafb77d6f1cb5836f86195684cdd123182f9f
shyjun <n.shyju@gmail.com>
2025-02-22 17:24:44 +0530

    updated with grep support, udp dbg support

--- main.c
+++ main.c
@@ -4,31 +4,47 @@
 #include <stdio.h>
 #include <errno.h>
 #include <assert.h>
+#include <regex.h>
 
 // Debug options. To enable debug, enable the next macro
 #define DBG_PRINT(x) do { } while(0)
 //#define DBG_PRINT(x) do { x ; } while(0)
 
 #define MAX_OPTIONS 100
-#define MAX_INPUT_LENGTH 3  // Maximum length for numeric input (e.g., "12" or "123")
+#define MAX_INPUT_LENGTH 100  // Maximum length for numeric input (e.g., "12" or "123")
 
 char *options[MAX_OPTIONS];  // Array to store options
 int num_options = 0;  // Number of options
 char prompt[256];  // Buffer to store the prompt
 int selected[MAX_OPTIONS] = {0};  // Array to track selected options
 int multi_select_enabled = 0;  // Multi-select disabled by default
-int default_selected_idx = 0;  // when loading, this should be the default selected
 int from_pipe = 0;
 char *in_file = NULL;
 char *out_file = NULL;
+int multi_select = 0;
+WINDOW *menu_win;
+int highlight = 1;
+char input_buffer[MAX_INPUT_LENGTH] = {0};  // Buffer to store input
+int input_length = 0;  // Length of the current input in the buffer
+int g_pressed = 0;  // State to track if 'g' was pressed
+int ch;
+int grep_mode = 0;
+regex_t regex;
+int ctrl_p = 0;
+int ctrl_n = 0;
+
+
+void udp_dbg(const char *fmt, ...);
+void set_udp_port(int port);
 
 void usage(char *app_name)
 {
-    fprintf(stderr, "Usage: %s [in_file=<input_file>] [out_file=<output_file>] [multi_select=yes] [default=<value>] [from_pipe=yes/no] [-h  for help]\n", app_name);
+    fprintf(stderr, "Usage: %s [in_file=<input_file>] [out_file=<output_file>] [multi_select=yes] [default=<value>] [from_pipe=yes/no] [udp_dbg_port=<udp_dbg_server_port>] [-h  for help]\n", app_name);
 }
 
 // Function to calculate the maximum width needed for the window
-int calculate_max_width() {
+int calculate_max_width()
+{
     int max_width = 0;
     int i;
 
@@ -51,7 +67,8 @@ int calculate_max_width() {
 }
 
 // Function to display the options with a border, serial numbers, and a prompt
-void display_menu(WINDOW *menu_win, int highlight) {
+void display_menu()
+{
     int i;
 
     // Clear the window
@@ -91,14 +108,25 @@ void display_menu(WINDOW *menu_win, int highlight) {
         wattroff(menu_win, A_REVERSE);  // Remove the highlight
     }
 
+    if(grep_mode == 1) {
+        /* if (input_length > 0) */
+        {
+            char grep_buff[300];
+            sprintf(grep_buff, "Grep:/%s",input_buffer);
+            mvwprintw(menu_win, i + 3, 2,"%s", grep_buff);
+        }
+    }
+
     refresh();
     // Refresh the window to show changes
     wrefresh(menu_win);
     curs_set(0);  // Hide the cursor
+
 }
 
 // Function to load options from a file
-void load_options() {
+void load_options()
+{
     FILE *file, *pipe_write_file;
 
     if(from_pipe == 1) {
@@ -164,10 +192,276 @@ void load_options() {
     fclose(file);
 }
 
-int main(int argc, char *argv[]) {
+void regex_find()
+{
+    int first_match_idx = -1;
+    int last_match_idx;
+    int found_highlight = -1;
+    int prev_highlight=-1;
 
-    int multi_select = 0;
+    // Compile the regular expression
+    int ret = regcomp(&regex, input_buffer, REG_EXTENDED);
+    if (ret != 0) {
+        printf("Could not compile regex\n");
+        exit(1);
+    }
+
+    udp_dbg("pattern=%s\n", input_buffer);
+    if((ctrl_n == 1) && (highlight == num_options)) {
+        // last entry and still ctrl+n is pressed..
+        highlight = 0;
+    }
+    int i;
+    for (i = 0; i < num_options; ++i) {
+        // Execute the regular expression
+        ret = regexec(&regex, options[i], 0, NULL, 0);
+        if (ret == 0) {
+            if(first_match_idx == -1) {
+                first_match_idx = i;
+            }
+            last_match_idx = i;
+            udp_dbg("%d:num_options=%d The text matches the pattern.highlight=%d,prev_highlight=%d\n",i,num_options,highlight,prev_highlight);
+            if(ctrl_n == 1) {
+                if(i <= highlight-1) { // item before current highlighted item
+                    continue;
+                }
+            }
+            if(ctrl_p == 1) {
+                if(highlight-1 == i) { // current highlighted item
+                    if(prev_highlight != -1) {
+                        found_highlight=prev_highlight;
+                    }
+                }
+                prev_highlight=i+1;
+                udp_dbg("%d: else ctrl_p.highlight=%d, prev_highlight=%d\n",i,highlight,prev_highlight);
+                continue;
+            }
+            highlight = i+1;
+            break;
+        } else if (ret == REG_NOMATCH) {
+            udp_dbg("%d: The text does not match the pattern.\n",i);
+        } else {
+            char error_message[100];
+            regerror(ret, &regex, error_message, sizeof(error_message));
+            udp_dbg("Regex match failed: %s\n", error_message);
+        }
+    }
+    if((ctrl_p == 1) && (first_match_idx != -1)) {
+        udp_dbg("ctrl+p end: i=%d:num_options=%d .highlight=%d,prev_highlight=%d,found_highlight=%d\n",
+                i,num_options,highlight,prev_highlight,found_highlight);
+        if(i == num_options) {
+            if(found_highlight != -1) {
+                highlight = found_highlight;
+            } else {
+                highlight = last_match_idx+1;
+            }
+        } else {
+            highlight = prev_highlight;
+        }
+    }
+
+    assert(highlight >= 0);
+
+    // Free the compiled regular expression
+    regfree(&regex);
+}
+
+int handle_grep_mode()
+{
+    ctrl_p = 0;
+    ctrl_n = 0;
+
+    if(ch == 27) { // ESC
+        input_length = 0;  // Clear the input buffer
+        input_buffer[0] = '\0';
+        grep_mode = 0;
+        return true;
+    }
+
+    udp_dbg("before: grep_mode:ch:%d,input_length=%d\n",ch,input_length);
+
+    if((ch == 8) || (ch == 263)) { // BackSpace
+        ch = 0;
+        if(input_length == 0) {
+            input_buffer[input_length] = '\0';  // Null-terminate the buffer
+        } else {
+            input_length --;
+            udp_dbg("BS: input_length=%d",input_length);
+        }
+        input_buffer[input_length] = '\0';  // Null-terminate the buffer
+    } else if (ch == 32) {  // Space key to select/unselect
+        if (!multi_select_enabled) {
+            memset(selected, 0, sizeof(selected));  // no multi select support. clear all selection
+        }
+        selected[highlight - 1] = !selected[highlight - 1];  // Toggle selection
+        input_length = 0;  // Clear the input buffer
+        input_buffer[0] = '\0';
+        g_pressed = 0;
+    } else if (ch == 10) {  // Enter key
+        if (!multi_select_enabled) {
+            memset(selected, 0, sizeof(selected));  // no multi select support. clear all selection
+            selected[highlight - 1] = !selected[highlight - 1];  // Toggle selection
+            return false;
+        } else {
+            selected[highlight - 1] = !selected[highlight - 1];  // Toggle selection
+        }
+        udp_dbg("Grep mode: Enter key");
+    } else if (ch == 16) {  // ctrl+p : prev
+        udp_dbg("Grep mode: ctrl+p key");
+        ctrl_p = 1;
+        ctrl_n = 0;
+    } else if (ch == 14) {  // ctrl+n : next
+        udp_dbg("Grep mode: ctrl+n key");
+        ctrl_p = 0;
+        ctrl_n = 1;
+    } else if(ch == '/') { // already in grep mode.
+        input_length = 0;  // Clear the input buffer
+        input_buffer[0] = '\0';
+    } else {
+        if (input_length < MAX_INPUT_LENGTH - 1) {
+            input_buffer[input_length++] = ch;  // Add digit to the buffer
+            input_buffer[input_length] = '\0';  // Null-terminate the buffer
+        }
+    }
+
+    udp_dbg("after: grep_mode:ch:%d,input_length=%d\n",ch,input_length);
+
+    regex_find();
+
+    // always continue
+    return true;
+}
+
+int handle_normal_mode()
+{
+    if(ch == '/') { // switch to grep mode
+        grep_mode = 1;
+        input_length = 0;
+        return true;
+    }
+
+    if(ch == 27)
+        return false;
+
+    if (ch >= '0' && ch <= '9') {  // Check if the key is a digit
+        if (input_length < MAX_INPUT_LENGTH - 1) {
+            input_buffer[input_length++] = ch;  // Add digit to the buffer
+            input_buffer[input_length] = '\0';  // Null-terminate the buffer
+
+            // Convert input buffer to an integer
+            int input_number = atoi(input_buffer);
+
+            // Validate and set highlight if input is within range
+            if (input_number >= 1 && input_number <= num_options) {
+                highlight = input_number;
+            }
+
+            // If the number is a single digit and valid, highlight immediately
+            if (input_length == 1 && input_number >= 1 && input_number <= num_options) {
+                highlight = input_number;
+                input_length = 0;  // Clear the input buffer after immediate selection
+                input_buffer[0] = '\0';
+            }
+        }
+    } else if (ch == 'k' || ch == KEY_UP || ch == 16) {  // Key 'k' or UP or CTRL+p for up
+        if (highlight == 1) {
+            highlight = num_options;
+        } else {
+            --highlight;
+        }
+        input_length = 0;  // Clear the input buffer
+        input_buffer[0] = '\0';
+        g_pressed = 0;
+    } else if (ch == 'j' || ch == KEY_DOWN || ch == 14) {  // Key 'j' or DOWN or CTRL+n for down
+        if (highlight == num_options) {
+            highlight = 1;
+        } else {
+            ++highlight;
+        }
+        input_length = 0;  // Clear the input buffer
+        input_buffer[0] = '\0';
+        g_pressed = 0;
+    } else if (ch == 4) {  // Ctrl+D
+        highlight += 4;
+        if (highlight > num_options) {
+            highlight = num_options;
+        }
+        input_length = 0;  // Clear the input buffer
+        input_buffer[0] = '\0';
+        g_pressed = 0;
+    } else if (ch == 21) {  // Ctrl+U
+        highlight -= 4;
+        if (highlight < 1) {
+            highlight = 1;
+        }
+        input_length = 0;  // Clear the input buffer
+        input_buffer[0] = '\0';
+        g_pressed = 0;
+    } else if (ch == KEY_NPAGE) {  // Page Down key
+        highlight += 8;
+        if (highlight > num_options) {
+            highlight = num_options;
+        }
+        input_length = 0;  // Clear the input buffer
+        input_buffer[0] = '\0';
+        g_pressed = 0;
+    } else if (ch == KEY_PPAGE) {  // Page Up key
+        highlight -= 8;
+        if (highlight < 1) {
+            highlight = 1;
+        }
+        input_length = 0;  // Clear the input buffer
+        input_buffer[0] = '\0';
+        g_pressed = 0;
+    } else if (ch == 'G') {  // 'G' key to go to the last item
+        highlight = num_options;
+        input_length = 0;  // Clear the input buffer
+        input_buffer[0] = '\0';
+        g_pressed = 0;
+    } else if (ch == 'g') {  // 'g' key to start sequence for 'gg'
+        if (g_pressed) {  // If 'g' was already pressed
+            highlight = 1;  // Go to the first item
+        }
+        g_pressed = 1;  // Set the state to indicate 'g' was pressed
+    } else if (ch == 32) {  // Space key to select/unselect
+        if (!multi_select_enabled) {
+            memset(selected, 0, sizeof(selected));  // no multi select support. clear all selection
+        }
+        selected[highlight - 1] = !selected[highlight - 1];  // Toggle selection
+        input_length = 0;  // Clear the input buffer
+        input_buffer[0] = '\0';
+        g_pressed = 0;
+    } else if (ch == 10) {  // Enter key
+        if (!multi_select_enabled) {
+            memset(selected, 0, sizeof(selected));  // no multi select support. clear all selection
+            selected[highlight - 1] = !selected[highlight - 1];  // Toggle selection
+        }
+
+        return false;  // Exit loop if single selection mode
+    }
+
+    return true;
+}
+
+int handle_keys()
+{
+    int again;
+    do
+    {
+        ch = getch();
+        if(grep_mode == 1) {
+            again = handle_grep_mode();
+        } else {
+            again = handle_normal_mode();
+        }
+        display_menu();
+    } while(again == true);
+}
+
+int main(int argc, char *argv[])
+{
     char *default_value = NULL;
+    int default_selected_idx = 0;  // when loading, this should be the default selected
 
     // Iterate through each argument
     for (int i = 1; i < argc; i++) {
@@ -182,6 +476,9 @@ int main(int argc, char *argv[]) {
             if (strcmp(argv[i] + 13, "yes") == 0) {
                 multi_select = 1;
             }
+        } else if (strncmp(argv[i], "udp_dbg_port=", 13) == 0) {
+            default_value = argv[i] + 13; // Assign pointer to the default value
+            set_udp_port(atoi(default_value));
         } else if (strncmp(argv[i], "default=", 8) == 0) {
             default_value = argv[i] + 8; // Assign pointer to the default value
             default_selected_idx = atoi(default_value);
@@ -214,16 +511,8 @@ int main(int argc, char *argv[]) {
 
     multi_select_enabled = multi_select;
 
-    WINDOW *menu_win;
-    int ch;
-    int highlight = 1;
-    char input_buffer[MAX_INPUT_LENGTH] = {0};  // Buffer to store numeric input
-    int input_length = 0;  // Length of the current input in the buffer
-    int g_pressed = 0;  // State to track if 'g' was pressed
-
     // Load options from the specified file
     load_options();
-    const char *selected_filename = out_file;
 
     if(default_selected_idx != 0) {
         highlight = default_selected_idx;
@@ -246,111 +535,16 @@ int main(int argc, char *argv[]) {
     menu_win = newwin(height, max_width, start_y, start_x);
 
     // Display the menu immediately
-    display_menu(menu_win, highlight);
+    display_menu();
 
     // Reopen stdin as /dev/tty to read from the terminal
     freopen("/dev/tty", "r", stdin);
 
     int select_done = 0;
-    while ((ch = getch()) != 27) {  // Press 'Esc' to quit (ASCII 27)
-        if (ch >= '0' && ch <= '9') {  // Check if the key is a digit
-            if (input_length < MAX_INPUT_LENGTH - 1) {
-                input_buffer[input_length++] = ch;  // Add digit to the buffer
-                input_buffer[input_length] = '\0';  // Null-terminate the buffer
 
-                // Convert input buffer to an integer
-                int input_number = atoi(input_buffer);
+    handle_keys();
 
-                // Validate and set highlight if input is within range
-                if (input_number >= 1 && input_number <= num_options) {
-                    highlight = input_number;
-                }
-
-                // If the number is a single digit and valid, highlight immediately
-                if (input_length == 1 && input_number >= 1 && input_number <= num_options) {
-                    highlight = input_number;
-                    input_length = 0;  // Clear the input buffer after immediate selection
-                    input_buffer[0] = '\0';
-                }
-            }
-        } else if (ch == 'k' || ch == KEY_UP || ch == 16) {  // Key 'k' or UP or CTRL+p for up
-            if (highlight == 1) {
-                highlight = num_options;
-            } else {
-                --highlight;
-            }
-            input_length = 0;  // Clear the input buffer
-            input_buffer[0] = '\0';
-            g_pressed = 0;
-        } else if (ch == 'j' || ch == KEY_DOWN || ch == 14) {  // Key 'j' or DOWN or CTRL+n for down
-            if (highlight == num_options) {
-                highlight = 1;
-            } else {
-                ++highlight;
-            }
-            input_length = 0;  // Clear the input buffer
-            input_buffer[0] = '\0';
-            g_pressed = 0;
-        } else if (ch == 4) {  // Ctrl+D
-            highlight += 4;
-            if (highlight > num_options) {
-                highlight = num_options;
-            }
-            input_length = 0;  // Clear the input buffer
-            input_buffer[0] = '\0';
-            g_pressed = 0;
-        } else if (ch == 21) {  // Ctrl+U
-            highlight -= 4;
-            if (highlight < 1) {
-                highlight = 1;
-            }
-            input_length = 0;  // Clear the input buffer
-            input_buffer[0] = '\0';
-            g_pressed = 0;
-        } else if (ch == KEY_NPAGE) {  // Page Down key
-            highlight += 8;
-            if (highlight > num_options) {
-                highlight = num_options;
-            }
-            input_length = 0;  // Clear the input buffer
-            input_buffer[0] = '\0';
-            g_pressed = 0;
-        } else if (ch == KEY_PPAGE) {  // Page Up key
-            highlight -= 8;
-            if (highlight < 1) {
-                highlight = 1;
-            }
-            input_length = 0;  // Clear the input buffer
-            input_buffer[0] = '\0';
-            g_pressed = 0;
-        } else if (ch == 'G') {  // 'G' key to go to the last item
-            highlight = num_options;
-            input_length = 0;  // Clear the input buffer
-            input_buffer[0] = '\0';
-            g_pressed = 0;
-        } else if (ch == 'g') {  // 'g' key to start sequence for 'gg'
-            if (g_pressed) {  // If 'g' was already pressed
-                highlight = 1;  // Go to the first item
-            }
-            g_pressed = 1;  // Set the state to indicate 'g' was pressed
-        } else if (ch == 32) {  // Space key to select/unselect
-            if (!multi_select_enabled) {
-                memset(selected, 0, sizeof(selected));  // no multi select support. clear all selection
-            }
-            selected[highlight - 1] = !selected[highlight - 1];  // Toggle selection
-            input_length = 0;  // Clear the input buffer
-            input_buffer[0] = '\0';
-            g_pressed = 0;
-        } else if (ch == 10) {  // Enter key
-            if (!multi_select_enabled) {
-                memset(selected, 0, sizeof(selected));  // no multi select support. clear all selection
-                selected[highlight - 1] = !selected[highlight - 1];  // Toggle selection
-            }
-            break;  // Exit loop if single selection mode
-        }
-
-        display_menu(menu_win, highlight);
-    }
+    char *selected_filename = out_file;
 
     // Save selected options to the specified file
     FILE *selected_file = fopen(selected_filename, "w");
---
+++ udp_dbg_client.c
@@ -0,0 +1,56 @@
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#define SERVER_IP "127.0.0.1"
+#define SERVER_PORT 8050
+#define BUFFER_SIZE 1024
+
+int sockfd;
+struct sockaddr_in server_addr;
+char print_buff[500] = {0};
+int udp_dbg_enable = 0;
+int udp_server_port;
+
+void set_udp_port(int port)
+{
+    udp_dbg_enable = 1;
+    udp_server_port = port;
+}
+
+void udp_dbg(const char *fmt, ...)
+{
+    if(udp_dbg_enable == 0)
+        return;
+
+    static int inited = 0;
+    if(inited == 0) {
+        // Create UDP socket
+        if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+            perror("Socket creation failed");
+            exit(EXIT_FAILURE);
+        }
+
+        // Set up the server address structure
+        memset(&server_addr, 0, sizeof(server_addr));
+        server_addr.sin_family = AF_INET;
+        server_addr.sin_port = htons(SERVER_PORT);
+        server_addr.sin_addr.s_addr = inet_addr(SERVER_IP);
+        inited = 1;
+    }
+
+    va_list args;
+    va_start(args, fmt);
+    vsnprintf(print_buff, sizeof(print_buff), fmt, args);
+    va_end(args);
+
+    // Send message to server
+    if (sendto(sockfd, print_buff, strlen(print_buff), 0, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
+        perror("Send failed");
+        close(sockfd);
+        exit(EXIT_FAILURE);
+    }
+}
